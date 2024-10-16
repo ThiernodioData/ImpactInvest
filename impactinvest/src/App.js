b5f744a22cb733065b2f8c94fcf3abd049b4add2
TKumpta <amadoutidianediallo662@gmail.com>
2024-10-16 00:33:33 +0000

    Update App.js

--- impactinvest/src/App.js
+++ impactinvest/src/App.js
@@ -6,6 +6,7 @@ import HomePage from './pages/HomePage';
 import InvestPage from './pages/InvestPage';
 import FunctioningPage from './pages/FunctioningPage';
 import PortfolioPage from './pages/PortfolioPage';
+import ProjectDetailsPage from './pages/details';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/invest" element={<InvestPage />} />
         <Route path="/functioning" element={<FunctioningPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/project/:id" element={<ProjectDetailsPage />} />
       </Routes>
       <Footer /> {/* Footer should be inside Router */}
     </Router>
